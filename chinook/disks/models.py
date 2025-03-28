319f9fdfc3689fb37c4e11cc65211e7448cb988a
mathiasdub <mathias.dbss62@gmail.com>
2025-03-28 17:34:55 +0100

    début de l'implémentation vue non générique affichant la liste des albums

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -5,11 +5,17 @@ from django.db import models
 
 class Artist(models.Model):
     name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
  
  
 class Album(models.Model):
     title = models.CharField(max_length=200)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
        
 
 class Track(models.Model):
@@ -19,3 +25,6 @@ class Track(models.Model):
     bytes = models.IntegerField()
     unitPrice = models.FloatField()
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Album
+
+
+def index(request):
+    albums = Album.objects.all()
+    return render(request, 'disks/index.html', {'albums': albums})
