1647d3c91cb4558e24b2d40f250e741750d9809f
mathiasdub <mathias.dbss62@gmail.com>
2025-03-28 17:00:22 +0100

    création de la base de données via les migrations

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -4,21 +4,18 @@ from django.db import models
 
 
 class Artist(models.Model):
-    id = models.IntegerField()
     name = models.CharField(max_length=200)
  
  
 class Album(models.Model):
-    id = models.IntegerField()
     title = models.CharField(max_length=200)
-    artist = models.ForeignKey(Artist)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
        
 
 class Track(models.Model):
-    id = models.IntegerField()
     name = models.CharField(max_length=200)
     composer = models.CharField(max_length=200)
     milliseconds = models.IntegerField()
     bytes = models.IntegerField()
     unitPrice = models.FloatField()
-    album = models.ForeignKey(Album)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
